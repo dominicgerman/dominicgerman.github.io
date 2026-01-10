d7935857131ce9942c765c2926fd50157838ecff
Dominic German <dominicgerman@gmail.com>
2026-01-10 00:47:14 -0600

    deploy attempt 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://dominicgerman.github.io',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/dominicgerman.github.io' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
